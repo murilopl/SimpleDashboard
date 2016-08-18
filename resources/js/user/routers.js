0ded1c42f3cb4a024ad95f9edd13bf1bde0341ed
Murilo Parente <muriloparentel@gmail.com>
2016-08-18 11:09:44 -0300

    URL Fixs

--- resources/js/user/routers.js
+++ resources/js/user/routers.js
@@ -10,25 +10,25 @@
 				.state('user', {
 					abstract: true,
 					url: '/user',
-					templateUrl: 'resources/views/user/base.html',
+					templateUrl: './resources/views/user/base.html',
 					controller: 'UserController'
 				})
 					.state('dashboard', {
 						url: '/dashboard',
 						parent: 'user',
-						templateUrl: 'resources/views/user/dashboard/dashboard.html',
+						templateUrl: './resources/views/user/dashboard/dashboard.html',
 						controller: 'DashboardController'
 					})
 					.state('overview', {
 						url: '/meeting/overview',
 						parent: 'dashboard',
-						templateUrl: 'resources/views/user/dashboard/pages/meeting/overview.html',
+						templateUrl: './resources/views/user/dashboard/pages/meeting/overview.html',
 						controller: 'OverviewController'
 					})
 					.state('about', {
 						url: '/meeting/about',
 						parent: 'dashboard',
-						templateUrl: 'resources/views/user/dashboard/pages/meeting/about.html',
+						templateUrl: './resources/views/user/dashboard/pages/meeting/about.html',
 						controller: 'AboutController'
 					})
 
